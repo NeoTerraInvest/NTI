bfd7ba2f5b35ab7d78bc039eb719644d737dc2d8
4BFC <4bee.code@gmail.com>
2025-02-11 14:13:34 +0900

    feat: implemented the main develop branch about Home.tsx and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Debug from "./components/Debug";
 import Test from "./components/Test";
+import Home from "./page/Home";
 
 const App = () => {
   return (
     <Routes>
-      <Route index path="/" element={<Debug />} />
+      <Route index path="/" element={<Home />} />
+      <Route index path="/debug" element={<Debug />} />
       <Route index path="/test" element={<Test />} />
     </Routes>
   );
