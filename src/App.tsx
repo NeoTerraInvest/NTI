6d1e1c5902f96358d1752a6a709900f6ddea72a4
4Bee <4bee.code@gmail.com>
2025-02-11 23:16:13 +0900

    Error: resolved `No routes matched location "test"` Error
    - due to `Test.tsx`file, specifically in the Hydration check, the syntax disturbs the router. Now, have a question: where was the Hydration error resolved?

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Test from "./components/Test";
 import Home from "./page/Home";
+import Error from "./components/Error";
 
 const App = () => {
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
       <Route path="/test" element={<Test />} />
+      <Route path="*" element={<Error statusCode={404} />} />
     </Routes>
   );
 };
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,15 +1,33 @@
-import { StrictMode } from "react";
-import { renderToString } from "react-dom/server";
+// Libraries imports
+import React from "react";
+import ReactDOMServer from "react-dom/server"; // Server-side rendering module from react-dom library.
 import { StaticRouter } from "react-router-dom/server";
-import App from "./App";
 
-export const render = (url: string) => {
-  const html = renderToString(
-    <StrictMode>
-      <StaticRouter location={url}>
+// App level imports
+import App from "./App";
+import Error, { ErrorProps } from "./components/Error";
+
+/**
+  This file uses ReactDOMServer, imported above, to render the application on the server-side. This is useful for SEO purposes, as it allows search engines to crawl the application and index its content. This is potentially useful for performance, as it allows the application to render faster on the client-side.
+ */
+
+interface IRenderProps extends ErrorProps {
+  path: string;
+  statusCode?: number;
+}
+
+export function render({ path, statusCode }: IRenderProps) {
+  if (statusCode) {
+    return ReactDOMServer.renderToString(<Error statusCode={statusCode} />);
+  }
+
+  const html = ReactDOMServer.renderToString(
+    // The renderToString method, is used to convert React components to an HTML string, which can be sent to the client for initial rendering.
+    <React.StrictMode>
+      <StaticRouter location={path}>
         <App />
       </StaticRouter>
-    </StrictMode>
+    </React.StrictMode>
   );
   return { html };
-};
+}
