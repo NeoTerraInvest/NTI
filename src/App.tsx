ed84009dd6c536a594ec7a71519ff5429385b75b
4BFC <4bee.code@gmail.com>
2025-02-20 17:45:15 +0900

    Error: `.env` didn't apply

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Test from "./components/dev/Test";
 import Home from "./page/Home";
 import Error from "./utils/Error";
 
+const testQeury = import.meta.env.VITE_TEST_QUERY;
+
 const App = () => {
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
-      <Route path="/test" element={<Test />} />
+      <Route path={`/test?${testQeury}`} element={<Test />} />
       <Route path="*" element={<Error statusCode={404} />} />
     </Routes>
   );
